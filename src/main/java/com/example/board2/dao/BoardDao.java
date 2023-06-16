5352592edc66ed29353de79d8e14ea52e5cc4b89
foxgeun <p31644@gmail.com>
2023-06-16 18:09:20 +0900

    “update”

---
+++ src/main/java/com/example/board2/dao/BoardDao.java
@@ -0,0 +1,11 @@
+package com.example.board2.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardDao {
+	
+	
+	
+	
+}
---
+++ src/main/java/com/example/board2/util/MyUtil.java
@@ -0,0 +1,11 @@
+package com.example.board2.util;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUtil {
+	
+	
+	
+	
+}
